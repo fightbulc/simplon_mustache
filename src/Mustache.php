35cbe8efeeef6216a6e90cfaf0129c71dc7ee6c9
Tino Ehrich <ehrich@efides.com>
2014-10-22 23:43:15 +0200

    init

---
+++ src/Mustache.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace Simplon\Mustache;
+
+/**
+ * Mustache
+ * @package Simplon\Mustache
+ * @author Tino Ehrich (tino@bigpun.me)
+ */
+class Mustache
+{
+    /**
+     * @var array
+     */
+    private static $data;
+
+    /**
+     * @param $template
+     * @param array $data
+     *
+     * @return string
+     */
+    public static function render($template, array $data)
+    {
+        self::$data = $data;
+        $template = self::parse($template, $data);
+
+        return self::cleanTemplate($template);
+    }
+
+    /**
+     * @param $template
+     * @param array $data
+     *
+     * @return string
+     */
+    protected static function parse($template, array $data)
+    {
+        foreach ($data as $key => $val)
+        {
+            if (is_array($val))
+            {
+                // find loops
+                preg_match_all('|{{#' . $key . '}}(.*?){{/' . $key . '}}|sm', $template, $foreachPattern);
+
+                // handle loops
+                if (isset($foreachPattern[1][0]))
+                {
+                    foreach ($foreachPattern[1] as $patternId => $patternContext)
+                    {
+                        $loopContent = '';
+
+                        // handle array objects
+                        if (isset($val[0]))
+                        {
+                            foreach ($val as $loopVal)
+                            {
+                                $loopContent .= self::parse($patternContext, $loopVal);
+                            }
+                        }
+
+                        // normal array only
+                        else
+                        {
+                            $loopContent = self::parse($patternContext, $val);
+                        }
+
+                        // replace pattern context
+                        $template = preg_replace(
+                            '|' . preg_quote($foreachPattern[0][$patternId]) . '|s',
+                            $loopContent,
+                            $template,
+                            1
+                        );
+                    }
+                }
+            }
+
+            // ----------------------------------
+
+            elseif (is_bool($val))
+            {
+                // determine true/false
+                $conditionChar = $val === true ? '\#' : '\^';
+
+                // find bools
+                preg_match_all('|{{' . $conditionChar . $key . '}}(.*?){{/' . $key . '}}|s', $template, $boolPattern);
+
+                // handle bools
+                if (isset($boolPattern[1][0]))
+                {
+                    foreach ($boolPattern[1] as $patternId => $patternContext)
+                    {
+                        // parse and replace pattern context
+                        $template = preg_replace(
+                            '|' . preg_quote($boolPattern[0][$patternId]) . '|s',
+                            self::parse($patternContext, self::$data),
+                            $template,
+                            1
+                        );
+                    }
+                }
+            }
+
+            // ----------------------------------
+
+            elseif ($val instanceof \Closure)
+            {
+                // set closure return
+                $template = str_replace('{{' . $key . '}}', $val(), $template);
+            }
+
+            // ----------------------------------
+
+            else
+            {
+                // set vars
+                $template = str_replace('{{' . $key . '}}', $val, $template);
+            }
+        }
+
+        return (string)$template;
+    }
+
+    /**
+     * @param string $template
+     *
+     * @return string
+     */
+    protected static function cleanTemplate($template)
+    {
+        // remove left over wrappers
+        $template = preg_replace('|{{.*?}}.*?{{/.*?}}\n*|s', '', $template);
+
+        // remove left over variables
+        $template = preg_replace('|{{.*?}}\n*|s', '', $template);
+
+        return (string)$template;
+    }
+}
